06da6bc97e0c927ec85bde3a4e0a753be9cb28c2
Rob B <rob@rjrobbie.dev>
2024-01-17 21:19:25 +0000

    updated the generatePassword function by using a for loop to call up to 8 random numbers

--- index.js
+++ index.js
@@ -5,8 +5,15 @@ let password2 = document.getElementById("password2-el");
 let generatePassword = document.getElementById("generatePassword");
 
 generatePassword.addEventListener("click", function () {
-  let randomPasswordOne = Math.floor(Math.random() * characters.length);
-  let randomPasswordTwo = Math.floor(Math.random() * characters.length);
-  password1.textContent = characters[randomPasswordOne];
-  password2.textContent = characters[randomPasswordTwo];
+  let randomPasswordOne = ""
+  let randomPasswordTwo = "" 
+
+  for (let i = 0; i < 8; i++) {
+  randomPasswordOne += characters[Math.floor(Math.random() * characters.length)]
+  randomPasswordTwo += characters[Math.floor(Math.random() * characters.length)]
+  }
+  
+  password1.textContent = randomPasswordOne
+  password2.textContent = randomPasswordTwo
 });
+
